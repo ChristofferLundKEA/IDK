5a74de07f0c25a26a2d2a190e20a755f538b3648
ChristofferLundKEA <chlu0002@stud.kea.dk>
2024-05-07 16:35:34 +0200

    MemberList update:hasPaidStatus

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,6 @@ public class Main {
         Controller.createNewMember();
         MemberList.listWhoNeedsToPay();
         MemberList.retrieveMemberInfoFromMemberID();
+        MemberList.editMemberHasPaidStatus();
     }
 }
--- src/MemberList.java
+++ src/MemberList.java
@@ -10,13 +10,24 @@ public class MemberList {
                     + thisMember.surName + "| ID:" + thisMember.memberID);
         }
     }
+
     public static void retrieveMemberInfoFromMemberID(){
         int input = InputManager.takeScannerInt("What memberID do you want information on?");
         for(Member thisMember : memberList){
             if (thisMember.memberID == input) System.out.println(thisMember);
         }
     }
+
     public static void editMemberHasPaidStatus(){
         int idtochange = InputManager.takeScannerInt("Which ID do you want to update payment status on?");
+        boolean found = false;
+        for (int i = 0; i < MemberList.memberList.size(); i++){
+            if (MemberList.memberList.get(i).memberID == idtochange) {
+                found = true;
+                    if(MemberList.memberList.get(i).hasPaid) MemberList.memberList.get(i).hasPaid = false; // toggle to opposite
+                    else MemberList.memberList.get(i).hasPaid = true; // toggle to opposite
+            }
+        }
+        if (!found) System.out.println("No such ID found");
     }
 }
